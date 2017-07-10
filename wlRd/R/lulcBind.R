9dccd5aa7318d4d83c70b02798351c43d9aa6fb6
kingcountyspb <steven.brady@kingcounty.gov>
2017-07-10 16:00:37 -0700

    Add files via upload

--- wlRd/R/lulcBind.R
+++ wlRd/R/lulcBind.R
@@ -1,19 +1,10 @@
-lulcBind <- function(file.path, score.type = NULL, ambient = F) {
-
-# loop through file paths, define anonymous function
-all.list <- lapply(1:length(file.path), function(x) {
-
-path.files <- list.files(file.path[x])
-
-# read in files
-list.with.each.file <- lapply(paste(file.path, list.files(file.path), sep = ''), function(y) read.csv(y, header=TRUE))
-
-# bind all files from each file.path into data frames
-do.call("rbind.data.frame", list.with.each.file)
-})
-
-# bind files from all file.paths into one data frame
-lulc <- do.call("rbind.data.frame", all.list)
-
-return(lulc)
-}
+lulcBind <- function(file.path){
+  # get list of files in path specified by user
+  path.files <- list.files(file.path)
+  # read in all the files, assign a new column that stamps on the file name, and bind altogether
+  do.call("rbind", lapply(paste(file.path, path.files, sep = ""), function(x) {
+    dat <- read.csv(x, header = T)
+    dat$filename <- tools::file_path_sans_ext(basename(x))
+    dat
+    }))
+  }
