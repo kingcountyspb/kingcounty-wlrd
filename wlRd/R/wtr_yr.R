410579a52ea030b78904f0e6ef2ecf7aeea3dc1f
kingcountyspb <steven.brady@kingcounty.gov>
2017-07-11 09:16:36 -0700

    Add files via upload

---
+++ wlRd/R/wtr_yr.R
@@ -0,0 +1,10 @@
+wtr_yr <- function(dates, start_month=9) {
+  # Convert dates into POSIXlt
+  dates.posix = as.POSIXlt(dates)
+  # Year offset
+  offset = ifelse(dates.posix$mon >= start_month - 1, 1, 0)
+  # Water year
+  adj.year = dates.posix$year + 1900 + offset
+  # Return the water year
+  adj.year
+}
